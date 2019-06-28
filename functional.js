c839cbd8399b4bf42b94b89d199ff86f5541b815
bhaines <bhaines@yext.com>
2019-06-28 04:48:53 -0400

    wip

--- functional.js
+++ functional.js
@@ -1,10 +1,8 @@
 ////////////////
 // Helpers
 ////////////////
-const l = R.curry((tag, x) => {console.log(tag, x); return x});
 const qsAll = R.invoker(1, 'querySelectorAll');
 const qs = R.invoker(1, 'querySelector');
-const arrayFrom = x => Array.from(x);
 const addClass = R.curry((cls, el) => {
   el.classList.add(cls);
 });
@@ -24,44 +22,29 @@ const OnReady = (cb) => {
 ////////////////
 // OOP Version
 ///////////////
-class OOPTabber {
+class Tabber {
   constructor (el) {
     this.el = el;
-
-    this.buttons = this.el.querySelectorAll('.js-Tabber-button');
-    this.contents = this.el.querySelectorAll('.js-Tabber-body');
-
     this.tabs = [];
-    this.currentActiveTab = null;
 
-    for (const button of this.buttons) {
-      for (const content of this.contents) {
+    const buttons = this.el.querySelectorAll('.js-Tabber-button');
+    const contents = this.el.querySelectorAll('.js-Tabber-body');
+    for (const button of buttons) {
+      for (const content of contents) {
         if (button.dataset.tabIndex === content.dataset.tabIndex) {
-          this.tabs.push({
-            'button': button,
-            'content': content,
-          });
+          this.tabs.push({button, content});
         }
       }
     }
 
     for (const tab of this.tabs) {
       tab.button.addEventListener('click', () => {
-        if (tab !== this.currentActiveTab) {
-          this.activateAndUpdateAll(tab);
-        }
+        this.activateAndUpdateAll(tab);
       });
-
-      if (!tab.button.classList.contains('is-active')) {
-        this.deactivateTab(tab);
-      } else {
-        this.currentActiveTab = tab;
-      }
     }
   }
 
   activateTab(tab) {
-    this.currentActiveTab = tab;
     tab.button.classList.add('is-active');
     tab.content.classList.add('is-active');
   }
@@ -88,20 +71,18 @@ const [activateEl, deactivateEl] = (R.map(f => f('is-active')))([addClass, remov
 const getTabIndex = R.path(['dataset', 'tabIndex']);
 const tabIndexEq = R.curry((idx, el) => getTabIndex(el) === idx);
 const activateElsWithTabIndex = R.curry((els, idx) => els.filter(tabIndexEq(idx)).map(activateEl));
-const deactivateElsWithoutTabIndex = R.curry((els, idx) => els.filter(R.compose(R.not, tabIndexEq(idx))).map(deactivateEl));
+const deactivateElsWithoutTabIndex = R.curry((els, idx) => els.filter((el) => !tabIndexEq(idx, el)).map(deactivateEl));
 
 const initTabber = el => {
-  const tabEls = [buttons, contents] = R.map(R.compose(Array.from, R.flip(qsAll)(el)))(['.js-Tabber-button', '.js-Tabber-body'])
-  const toRun = R.ap([activateElsWithTabIndex, deactivateElsWithoutTabIndex], tabEls)
-  const handler = R.compose(R.juxt(toRun), getTabIndex)
-
+  const tabEls = [buttons, contents] = R.map(R.compose(Array.from, R.flip(qsAll)(el)))(['.js-Tabber-button', '.js-Tabber-body']);
+  const toRun = R.ap([activateElsWithTabIndex, deactivateElsWithoutTabIndex], tabEls);
+  const handler = R.compose(R.juxt(toRun), getTabIndex);
   Array.from(buttons).forEach(el => el.addEventListener('click', () => handler(el)));
 }
 
-
 OnReady(() => {
   // Init OOP Tabber
-  const oopTabber = new OOPTabber(document.querySelector('.js-oop-tabber'));
+  new Tabber(document.querySelector('.js-oop-tabber'));
 
   // Init FP Tabber
   initTabber(qs('.js-fp-tabber')(document));
