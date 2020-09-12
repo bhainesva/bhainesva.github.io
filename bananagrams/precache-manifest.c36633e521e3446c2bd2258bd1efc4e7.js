f2d725f6ed3d9f20abe1a51e269fee3cd4f69c34
bhaines <bhaines@yext.com>
2020-09-12 03:53:47 -0400

    bananagrams demo

---
+++ bananagrams/precache-manifest.c36633e521e3446c2bd2258bd1efc4e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c67600bb78086bd62f967295f1b59ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "994b3ea64719cf975f37",
+    "url": "/static/css/main.8302bba3.chunk.css"
+  },
+  {
+    "revision": "49da7777f6a16e5d9736",
+    "url": "/static/js/2.abc1674a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.abc1674a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994b3ea64719cf975f37",
+    "url": "/static/js/main.5eeb3a1d.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  }
+]);
