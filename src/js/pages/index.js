91575e6cf23bf7952f682300e4bc6b510d909d63
Benjamin Haines <bhaines@yext.com>
2021-04-22 01:47:14 -0400

    add dragon cookie

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -2,8 +2,20 @@ import { onReady } from '../util';
 import '../../index.html';
 import '../../index.css';
 
+function doIfNoCookie(fn) {
+  if (!document.cookie.split('; ').find(row => row.startsWith('dragonCookie'))) {
+    fn();
+    const date = new Date()
+    date.setDate(date.getDate() + 1);
+    document.cookie = `dragonCookie=true; expires=${date.toUTCString()}`;
+  }
+}
+
 onReady(() => {
   const interstitial = document.querySelector('.js-interstitial');
+
+  doIfNoCookie(() => interstitial.classList.remove('is-hidden'));
+
   interstitial.addEventListener('click', () => {
     interstitial.innerText = 'slick';
     window.setTimeout(() => interstitial.classList.toggle('is-dismissed'), 300);
